5e4cba0dc135486ae3f1577d387fdb0760451a13
netrahindocha <netrahindocha1@gmail.com>
2022-10-22 16:53:48 +0530

    Added navbar styling and created responsive navbar

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav(){
+    let options = document.getElementById('options');
+    if(options.style.display === "none"){
+        options.style.display = "block";
+    }
+    else{
+        options.style.display = "none";
+    }
+    
+}
+
